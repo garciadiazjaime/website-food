7bf27ac1ee6f54cceb5b272ed01b6f8163bbb8e2
jaime <jgarciadiaz@groupon.com>
2020-04-04 13:26:15 -0500

    feat: connect to api

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -33,20 +33,110 @@
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>...</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+{#if posts }
+	{#each posts as post}
+	<Card style="width: 360px;">
+		<PrimaryAction on:click={() => doAction('openItemPage')}>
+			
+			<Media style="background-image: url({getImageURL(post)});" aspectRatio="16x9">
+			</Media>
+			<Content class="mdc-typography--body2">
+				{post.caption}
+			</Content>
 
-<figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
-
-<Button on:click={() => alert('Clicked!')}>Just a Button</Button>
+		</PrimaryAction>
+	</Card>
+	{/each}
+{/if}
 
 <script>
-  import Button from '@smui/button';
+	import Card, { Content, PrimaryAction, Media } from '@smui/card';
+  import IconButton, {Icon} from '@smui/icon-button';
+	
+	import { onMount } from 'svelte';
+
+	let posts
+
+	function getPlacesQuery() {
+		return `
+				query Post {
+					posts {
+						_id
+						commentsCount
+						permalink
+						mediaType
+						mediaUrl
+						caption
+						id
+						likeCount
+						children {
+							id
+							media_type
+							media_url
+							caption
+						}
+						city
+						source
+						state
+						createdAt
+						updatedAt
+					}
+				}
+			`;
+	}
+
+	async function getPosts() {
+		const payload = {
+			query: getPlacesQuery()
+		};
+
+		const result = await fetch(`http://api.mintitmedia.com/instagram/graphiql`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(payload)
+		});
+
+		const {
+			data: { posts }
+		} = await result.json();
+
+		return posts;
+	}
+
+	onMount(async () => {
+		const data = await getPosts()
+
+		posts = data
+	});
+
+	function getType(type) {
+		if (type === 'CAROUSEL_ALBUM') {
+			return "Album"
+		}
+
+		return "Image"
+	}
+
+	function getImageURL(post) {
+		if (post.mediaType === 'IMAGE') {
+			return post.mediaUrl
+		}
+
+		if (Array.isArray(post.children) && post.children.length) {
+			return post.children[0].media_url
+		}
+
+		return "/default.png"
+	}
+
+	function doAction(action) {
+    console.log('You did an action: '+action);
+  }
+
+	export { posts };
 </script>
