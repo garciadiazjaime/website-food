dd5d63de6022792a6f2e77bb03f594ff885acceb
alex-romanov <alejandro@mintitmedia.com>
2020-04-13 11:47:19 -0700

    pre pull

--- src/components/Phones.svelte
+++ src/components/Phones.svelte
@@ -4,37 +4,41 @@
 </script>
 
 <style>
-  .phone-grid {
-		display: grid;
-    grid-column-gap: 6%;
-    grid-row-gap: 10px;
-		grid-template-columns: repeat( auto-fit, 47% );
-    margin: 14px 0 0;
-	}
-	a {
-    box-shadow: 0 3px 1px -2px rgba(0,0,0,.2), 0 2px 2px 0 rgba(0,0,0,.14), 0 1px 5px 0 rgba(0,0,0,.12);
+  a {
+    background-color: white;
+    border: 1px solid #ff5926;
+    color: #ff5926;
+    font-size: 12px;
     transition: box-shadow .28s cubic-bezier(.4,0,.2,1);
-    background-color: #ff5926;
-    color: rgba(255, 255, 255, .95);
-    display: inline-block;
-    padding: 11px 0;
-    width: 100%;
+    padding: 4px 0;
     text-decoration: none;
     text-align: center;
-    font-size: 15px;
     line-height: 1;
     font-weight: 400;
-    border-radius: 4px;
-    box-shadow: 1px 1px 5px rgba(0, 0, 0, .3);
   }
-  a:hover {
+	a.cta {
+    background-color: #ff5926;
+    box-shadow: 0 3px 1px -2px rgba(0,0,0,.2), 0 2px 2px 0 rgba(0,0,0,.14), 0 1px 5px 0 rgba(0,0,0,.12);
+    color: rgba(255, 255, 255, .95);
+    padding: 11px 0;
+    display: inline-block;
+    width: 80%;
+    display: block;
+    font-size: 15px;
+    border-radius: 4px;
+  }
+  a:first-child:hover {
     color: #fff;
   }
 </style>
 {#if phones}
   <div class="phone-grid">
-    {#each phones as phone}
-      <a href={`tel:${phone}`} variant="raised">{getPhoneNumber(phone)}</a>
+    {#each phones as phone, index}
+      {#if index === 0}
+        <a class="cta" href={`tel:${phone}`}>{getPhoneNumber(phone)}</a>
+      {/if}
+      {:else}
+       <a href={`tel:${phone}`}>{getPhoneNumber(phone)}</a>
     {/each}
   </div>
 {/if}
