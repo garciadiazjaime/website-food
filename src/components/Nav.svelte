8273a56452a67d668766a510fb32123493ecb9b7
jaime <garciadiazjaime@gmail.com>
2020-07-16 19:54:15 -0500

    feat: hide map and stats

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -3,12 +3,17 @@
 	export let segment;
 </script>
 
+<style>
+	.hide {
+		display: none;
+	}
+</style>
 <nav>
 	<ul>
 		<li><img class="logo" src="fmt_icon.svg" alt="Feed me Tj" height="40"/></li>
 		<li><a aria-current='{segment === undefined ? "page" : undefined}' href='.'>directorio</a></li>
 		<li><a aria-current='{segment === "about" ? "page" : undefined}' href='/about'>proyecto</a></li>
-		<li><a aria-current='{segment === "map" ? "page" : undefined}' href='/map'>mapa</a></li>
-		<li><a aria-current='{segment === "stats" ? "page" : undefined}' href='/stats'>datos</a></li>
+		<li class="hide"><a aria-current='{segment === "map" ? "page" : undefined}' href='/map'>mapa</a></li>
+		<li class="hide"><a aria-current='{segment === "stats" ? "page" : undefined}' href='/stats'>datos</a></li>
 	</ul>
 </nav>
