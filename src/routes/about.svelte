edbe3fcf84bce90ace478aaf3464cbdda15b16e4
alex-romanov <alejandro@mintitmedia.com>
2020-07-21 17:23:26 -0700

    what its all about

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -6,14 +6,13 @@
 </svelte:head>
 <div class="about-page">
 	<div class="title">
-		<h1>Feed Me Tj</h1>
+		<h1><img src="fmt-logo-gold.svg" alt="Feed Me Tj"></h1>
 	</div>
-	<div class="content-block primary">
+	<div class="content-block secondary">
 		<h2>¿Qué es Feed Me Tj?</h2>
 		<p>FMT es un directorio que hace muy fácil publicar y ver ofertas para comer cerca de tu ubicación.</p>
 		<p>Nuestro contenido viene de Instagram a través de hashtags que estamos observando. Si agregas <span class="fancy">#feedmetj</span> a tu post, aparecerá en FMT.</p>
 		<p>Nuestro objetivo es ofrecer un canal de promoción efectivo donde sea posible emprender sin necesidad de gastar ni tiempo ni dinero adicional.</p>
-
 
 		<h2>¿Cómo funciona FMT?</h2>
 		<ol>
@@ -29,9 +28,8 @@
 			<li>Porque Frescomer pone ese contenido enfrente de quienes lo están buscando específicamente.</li>
 		</ul>
 	</div>
-	<div class="content-block secondary">
+	<div class="content-block tertiary">
 		<h2>¿Cómo administrar tu perfil?</h2>
-
 		<p>Tu perfil es tu cuenta de Instagram, no necesitas crear una cuenta nueva.</p>
 		<ol>
 			<li>Incluye <span class="fancy">#feedmetj</span> para activar la cuenta de tu negocio en nuestra plataforma</li>
@@ -40,13 +38,14 @@
 		<p>Y no olvides, mientras más completa tengamos tu información, mejor ranking van a tener tus posts en nuestras listas de resultados</p>
 	</div>
 	<div class="content-block secondary">
-		<h2>¿Qué ganamos nosotros de todo esto?</h2>
+		<h2>¿Por qué estamos haciendo esto?</h2>
 		<p>
 			Este proyecto comenzó con la intención de facilitar que nuestra comunidad se adapte a la "nueva realidad".
-			Creemos que desaparecer las barreras técnicas y económicas para incorporarse a la economía digital, 
-			va a fomentar la supervivencia y creación de nuevos negocios. 
-			Nosotros seguiremos desarrollando features en la plataforma hasta que podamos decirle al restaurantero:
-			Tienes esto gratis, pero si inviertes tanto, se te va a regresar más. Más sencillo decirlo que hacerlo, pero el primer paso está claro y es publicar esta página.
+			Pensamos que desaparecer las barreras técnicas y económicas para incorporarse a la economía digital, 
+			va a fomentar la supervivencia y creación de nuevos negocios.
+		</p>
+		<p>
+			Creemos que conforme madure la comunidad de esta plataforma, lograremos identificar oportunidades de crecimiento para nosotros también.
 		</p>
 
 		<h2>¿Cuál es el siguiente paso para la plataforma?</h2>
