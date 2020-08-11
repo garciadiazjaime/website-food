67d7cc5717027f9a07a9e13cb9acaaa9e86c95ad
alex-romanov <alejandro@mintitmedia.com>
2020-08-11 14:43:18 -0700

    I think I fixed it

--- src/components/Profile.svelte
+++ src/components/Profile.svelte
@@ -1,17 +1,20 @@
 <script>
   import { onMount } from "svelte";
+  	import { tick } from 'svelte';
   import Dialog, { Title, Content, Actions, InitialFocus } from '@smui/dialog';
   import Button, { Label } from '@smui/button';
   import { profileId, profiles } from '../utils/stores';
   import './Profile.scss';
   let profileDialog;
-  let profile;
+  let profile = {};
+  let posts = [];
   
-  //const { title } = profiles.find(profile => profile.id == $profileId);
-  
-  export function openDialog() {
+  export async function openDialog() {
     profileDialog.open();
-    console.log($profiles);
+    await tick();
+    profile = $profiles.find(profile => profile.id == $profileId);
+    posts = profile.posts;
+    console.log(profile);
   }
 </script>
 
@@ -19,6 +22,10 @@
   <Title id="simple-title">{profile.title}</Title>
   <button class="close"on:click={() => profileDialog.close()} />
   <Content id="simple-content" aria-label="Mapa">
-     Content
+    {profile.dist}<br />
+    <img src={profile.mediaUrl} alt={profile.title} width="100"/>
+    {#each posts as post}
+      {post.caption} <br />
+    {/each}
   </Content>
 </Dialog>
