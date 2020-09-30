0eaf4697868b67a89582ca6c71edf6630f165b53
alex-romanov <alejandro@mintitmedia.com>
2020-09-29 20:46:29 -0700

    fixes de ale

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -8,65 +8,54 @@
 	<div class="title">
 		<h1><img src="fmt-logo-gold.svg" alt="Feed Me Tj"></h1>
 	</div>
+
 	<div class="content-block secondary">
 		<h2>¿Qué es Feed Me Tj?</h2>
-		<p>Feed me TJ es una aplicación que logra acercar a tu producto y los clientes potenciales. Por medio del monitoreo de hashtags en Instagram y la ubicación de usuarios, hace posible contestar la eterna
-		pregunta ¿qué hay cerca de mí? Por medio de respuestas inmediatas, gratuitas y visuales, propiciando la entrada de comensales a tu negocio.</p>
+		<p>
+			Feed me TJ es una aplicación que logra acercar a tu producto y los clientes potenciales. Por medio del monitoreo de hashtags en Instagram y la ubicación de usuarios, hace posible contestar la eterna pregunta ¿qué hay cerca de mí? Por medio de respuestas inmediatas, gratuitas y visuales, propiciando la entrada de comensales a tu negocio.
+		</p>
+	</div>
 
-		
-	     <div class="tertiary">
-		<h2>¿Cómo funciona FMTJ?</h2>
-	
-		 <ol>
+ 	<div class="content-block tertiary">
+		<h2>
+			¿Cómo funciona FMTJ?
+		</h2>
+		<ol>
 			<li>La configuración permite el monitoreo de hasthags relacionados con oferta de servicios.</li>
 			<li>Estos hasthags se vinculan a la información de la ubicación del usuario.</li>
-			<li>Con estos datos optimizamos FMTJ, optimiza resultados para darle la mejor oferta a los usuarios cerca de cada oferta en toda la ciudad.</li>
-			
-		 </ol>
-     
-		</div>
-		
+			<li>
+				Con estos datos optimizamos FMTJ, optimiza resultados para darle la mejor oferta a los usuarios cerca de cada oferta en toda la ciudad.
+			</li>	
+		</ol>
+	</div>
 
-	  
-
+	<div class="content-block secondary">
 		<h2>¿Por qué usar FMTJ?</h2>
 		<ul>
 			<li>Tu inversión en contenido es optimizada con geolocalización para que llegues a quienes serán tus clientes.</li>
 			<li>No requiere inversión adicional de ninguna de las partes.</li>
-			
 		</ul>
-	
+	</div>
 	   
-	
-	
-	
-	  <div class=" tertiary">
+	<div class="content-block tertiary">
 		<h2>¿Cómo administrar tu perfil?</h2>
-		<p>Contamos con un amplio directorio de servicios obtenido por medio de hasthags relacionados con comida. 
-		Si quieres ser parte de nuestro directorio y estar primero en la lista de resultados  solo tienes que utilizar nuestro hasthag #feedmetj  en un post que incluya información de contacto, ubicación y whatsap  y listo,  apareces como primera opción a usuarios cercanos a tu establecimiento, listos para hacer un pedido.</p>
+		<p>
+			Contamos con un amplio directorio de servicios obtenido por medio de hasthags relacionados con comida. Si quieres ser parte de nuestro directorio y estar primero en la lista de resultados  solo tienes que utilizar nuestro hasthag #feedmetj  en un post que incluya información de contacto, ubicación y whatsap  y listo,  apareces como primera opción a usuarios cercanos a tu establecimiento, listos para hacer un pedido.
+		</p>
+	</div>
 	
 	
-	  </div>
-	
-	
-	    <div class="secondary">
+	<div class="content-block secondary">
 		<h2>¿Por qué estamos haciendo esto?</h2>
 		<p>
 			Somos un grupo de desarrolladores tijuanenses, felices de habitar y disfrutar esta ciudad con todos los sabores y riquezas que ofrece en cada rincón y cada esquina. Cómo fronterizos sabemos que es difícil navegar la ciudad y encontrar su oferta, siempre cambiante y variada. Creemos que estos proyectos son dignos de ser descubiertos y que la relación con el comensal sólo se dá si saben mutuamente de su existencia. Por ello nos hemos esmerado en desparecer las barreras técnicas y económicas para ese encuentro y facilitarlo con las herramientas que todas las partes tienen a su alcance: Los teléfonos inteligentes permiten hacer ese match entre quién busca y quién ofrece, resultando en un ganar-ganar del que estamos seguros nos beneficiaremos nosotros de alguna manera.
 		</p>
+	</div>
 		
-	    </div>
-		
-		
-		<div class="tertiary">
+	<div class="content-block tertiary">
 		<h2>¿Cuál es el siguiente paso para la plataforma?</h2>
 		<p>
-			Queremos escuchar de tí productor, consumidor y promotor. Si tienes ideas, comentarios, quejas queremos saberlas para mejorar.
-			Escríbenos a <a class="fancy" href="mailto:info@feedmetj.com">info@feedmetj.com</a>
-	 </p>
-		   
-	</div>
-
-
+			Queremos escuchar de tí productor, consumidor y promotor. Si tienes ideas, comentarios, quejas queremos saberlas para mejorar. Escríbenos a <a class="fancy" href="mailto:info@feedmetj.com">info@feedmetj.com</a>
+	 </p>  
 	</div>
 </div>
