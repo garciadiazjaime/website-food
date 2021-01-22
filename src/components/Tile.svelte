7645cf451e59ecd86d740a3e398c4bac61dbd179
jaime <garciadiazjaime@gmail.com>
2021-01-22 11:50:21 -0600

    feat: use tile

---
+++ src/components/Tile.svelte
@@ -0,0 +1,62 @@
+<script>
+  export let place;
+
+  function clickHandler(place) {
+    window.open(`https://www.instagram.com/${place.username}/`, "_blank");
+  }
+</script>
+
+<style>
+  .tile {
+    padding: 6px 0;
+    box-shadow: 2px 2px 6px 6px #c8c8c8;
+    width: 100%;
+  }
+
+  .tile:hover {
+    cursor: pointer;
+  }
+
+  h2 {
+    margin: 0;
+    padding: 0 6px 12px;
+  }
+
+  img {
+    height: 180px;
+    min-height: 180px;
+    width: 100%;
+    object-fit: cover;
+  }
+
+  .keywords {
+    padding: 0 6px;
+  }
+
+  strong {
+    font-weight: normal;
+    display: inline-block;
+  }
+
+  strong::after {
+    content: '|';
+    display: inline-block;
+    padding: 0 6px;
+  }
+
+  strong:last-of-type::after {
+    content: '';
+  }
+</style>
+
+<div class="tile" on:click={() => clickHandler(place)}>
+  <h2>{place.title}</h2>
+
+  <img src={place.mediaUrl} alt={place.title} />
+
+  <div class="keywords">
+    {#each place.keywords as keyword}
+      <strong>{keyword}</strong>
+    {/each}
+  </div>
+</div>
--- src/routes/[category].svelte
+++ src/routes/[category].svelte
@@ -19,6 +19,7 @@
 </script>
 
 <script>
+  import Tile from '../components/Tile.svelte'
   export let title;
   export let options;
 
@@ -44,47 +45,6 @@
       grid-template-columns: repeat(auto-fit, minmax(24%, 1fr));
     }
   }
-
-  .card {
-    padding: 6px 0;
-    box-shadow: 2px 2px 6px 6px #c8c8c8;
-    width: 100%;
-  }
-
-  .card:hover {
-    cursor: pointer;
-  }
-
-  h2 {
-    margin: 0;
-    padding: 0 6px 12px;
-  }
-
-  img {
-    height: 180px;
-    min-height: 180px;
-    width: 100%;
-    object-fit: cover;
-  }
-
-  .keywords {
-    padding: 0 6px;
-  }
-
-  strong {
-    font-weight: normal;
-    display: inline-block;
-  }
-
-  strong::after {
-    content: '|';
-    display: inline-block;
-    padding: 0 6px;
-  }
-
-  strong:last-of-type::after {
-    content: '';
-  }
 </style>
 
 <svelte:head>
@@ -96,17 +56,7 @@
 
   <div class="grid">
     {#each options.data as option}
-      <div class="card" on:click={() => clickHandler(option)}>
-        <h2>{option.title}</h2>
-
-        <img src={option.mediaUrl} alt={option.title} />
-
-        <div class="keywords">
-          {#each option.keywords as keyword}
-            <strong>{keyword}</strong>
-          {/each}
-        </div>
-      </div>
+      <Tile place={option} />
     {/each}
   </div>
 </div>
