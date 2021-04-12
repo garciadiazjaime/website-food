8691986c0cf9863c962e68104c907f1ea63cf701
jaime <garciadiazjaime@gmail.com>
2021-04-12 17:54:50 -0500

    feat: add rifa

---
+++ src/routes/rifa.svelte
@@ -0,0 +1,122 @@
+<style>
+  .container {
+    padding: 12px;
+    max-width: 800px;
+    margin: 0 auto;
+    border: 1px dotted black;
+  }
+
+  .post {
+    background-color: white;
+    padding: 12px;
+    width: 400px;
+  }
+
+  .aggreatment {
+    height: 300px;
+    border: 1px solid black;
+    display: flex;
+    text-align: center;
+    padding: 12px;
+  }
+
+  .aggreatment span {
+    text-decoration: underline;
+  }
+
+  .aggreatment div {
+    flex: 1;
+    align-self: flex-end;
+  }
+</style>
+
+<div class="container">
+
+  <h1>Acuerdo para Rifa de $50 m.n.</h1>
+
+  <h2>Datos del Cliente</h2>
+  <p>
+    Cuenta: <b>cafed.alicia</b>
+    <br />
+    Dirección: <b>Tijuana, Baja California Colectivo VYS Showroom Plaza Alameda 22425</b>
+    <br />
+    Teléfono: <b>664 6742592</b>
+    <br />
+    Correo: <b>cafe.alicia@outlook.com</b>
+  </p>
+
+  <h2>
+    Compromisos del cliente:
+  </h2>
+  <ul>
+    <li>Hacer válido el monto de $50 m.n. a la persona que resulte ganadora de la rifa.</li>
+    <li>El monto se podrá utilizar como balance para la compra de cualquier producto que ofrezca el cliente.</li>
+    <li>El monto se utilizará en una única transacción.</li>
+    <li>El monto no generá saldo a favor, es decir no se dará cambio.</li>
+    <li>El monto se podrá utilizar hasta el 26 de Mayo del 2021.</li>
+  </ul>
+
+  <h2>Rifa</h2>
+
+  <p>
+    El lunes <b>19 de Abril del 2021</b>, la cuenta de <b>feedmetj</b> hará una publicación con el siguiente contenido:
+  </p>
+
+  <div class="post">
+    <p>
+      <img src="https://bxecreative.com/wp-content/uploads/2016/04/dummy-post-horisontal.jpg" width="400px" alt="imagen dummy" />
+    </p>
+    <p>
+      Hola Comunidad de <b>#feedmetj</b>,
+
+      nuestros amigos de <b>@cafed.alicia</b> nos regalaron un Gift Card de <b>$50 m.n.</b>
+    </p>
+
+    <p>
+      El cual vamos a rifar el lunes <b>26 de Abril del 2021 a las 8pm</b>.
+    </p>
+
+    <p>Cómo participar?</p>
+    <p>
+      Fácil, solo deja un comentario en esta publicación etiquetando a un amigo.
+    </p>
+
+    <p>
+      Para tener más posibilidades: <br />
+      -  Sigue a la cuenta: <b>@cafed.alicia</b> <br />
+      - Etiqueta a más amigos
+    </p>
+
+    <p>
+      Suerte!
+    </p>
+  </div>
+
+  <br>
+
+  <h2>Transacción</h2>
+
+  <ul>
+    <li>El usuario ganador tendrá un QR Code, el cual el cliente va escanear con la cámara de su teléfono.</li>
+    <li>El QR abrirá una página de Internet en el teléfono del cliente con los datos del ganador.</li>
+    <li>El cliente presionará el botón de "Aceptar", con lo cual quedará claro que el ganadaro ha usado su premio.</li>
+  </ul>
+
+  <p>
+    * Ejemplo de QR Code: <br />
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/1200px-QR_code_for_mobile_English_Wikipedia.svg.png" width="100px" alt="ejemplo de qr code" />
+  </p>
+
+  <br />
+  <h2>Acuerdo</h2>
+  <div class="aggreatment">
+    <div>
+        De acuerdo <br /><br /><br /><br /><br />
+        <span>Equipo de Feedmetj</span>
+    </div>
+    <div>
+      <span>@cafed.alicia</span>
+    </div>
+  </div>
+
+</div>
