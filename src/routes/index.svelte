073c3e4895eeebaf31c5b46e8f2ae7aea8fb900c
jaime <garciadiazjaime@gmail.com>
2021-10-08 13:08:24 -0500

    feat: sushi

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,7 @@
 <script>
-	
+	import TresLugaresBrunch from "../components/blog/3-lugares-brunch/intro.svelte"
+	import TresLugaresSushi from "../components/blog/3-lugares-sushi/intro.svelte"
+	import PanFrances from "../components/blog/pan-frances/intro.svelte"
 </script>
 
 <style>
@@ -30,11 +32,6 @@
 		margin: 0 auto;
 		padding-top: 20px;
 	}
-
-	img {
-		width: 100%;
-		height: auto;
-	}
 </style>
 
 <svelte:head>
@@ -50,100 +47,15 @@
 <div class="content">
 	<div class="item-list">
 		<ul>
-			<li>
-				<img src="/que-comer-en-tijuana/brunch/3-lugares-brunch-tijuana.png" 
-					alt="pan-frances" 
-					width="750"
-					height="938"
-				/>
-				<h2>3 lugares para hacer Brunch en Tijuana</h2>
-				<p>
-					La palabra Brunch viene de las palabras anglosajonas breakfast (desayuno) y lunch (almuerzo).
-				<p>
-					Lo cual queda perfecto para el día posterior a la fiesta o simplemente para aquellos días que 
-					preferimos despertar más tarde.
-				</p>
-				<p>
-					Tijuana como buena ciudad fronteriza, cuenta con excelentes opciones para "brunchear". Aquí te dejamos 3:
-				</p>
+			<li><TresLugaresSushi /></li>
 
-				<p>
-					1. Restaurante Cielo
-				</p>
-				<p>
-					Churro Waffles y Huevos Rancheros
-				</p>
-				<p>
-					<img src="/que-comer-en-tijuana/cielo.restaurante/churro-waffle-huevos-rancheros.jpeg" 
-						alt="Churro Waffles y Huevos Rancheros" 
-						width="750"
-						height="938"
-					/>
-				</p>
-				<p>
-					<a href="https://www.instagram.com/cielo.restaurante/" target="_blank" rel="nofollow noreferrer">@cielo.restaurante</a>
-				</p>
+			<li><hr /></li>
 
-				<p>
-					2. Folklor Café
-				</p>
-				<p>
-					Huevos, Frijoles y Chilaquiles.
-				</p>
-				<p>
-					<img src="/que-comer-en-tijuana/folklorcafe/desayuno.jpeg" 
-						alt="Desayuno Folklor Café" 
-						width="750"
-						height="938"
-					/>
-				</p>
-				<p>
-					<a href="https://www.instagram.com/folklorcafe/" target="_blank" rel="nofollow noreferrer">@folklorcafe</a>
-				</p>
+			<li><TresLugaresBrunch /></li>
 
-				<p>
-					3. Restaurante Buna
-				</p>
-				<p>
-					Omelette de la Casa con Chilaquiles
-				</p>
-				<p>
-					<img src="/que-comer-en-tijuana/buna/omelette-chilaquiles.jpeg" 
-						alt="Omelette y Chilaquiles" 
-						width="750"
-						height="938"
-					/>
-				</p>
-				<p>
-					<a href="https://www.instagram.com/bunatjmx/" target="_blank" rel="nofollow noreferrer">@bunatjmx</a>
-				</p>
-			</li>
+			<li><hr /></li>
 
-			<li>
-				<hr />
-			</li>
-
-			<li>
-				<img src="/que-comer-en-tijuana/citron.mx/pan-frances.jpeg" 
-					alt="pan frances tijuana" 
-					width="750"
-					height="938"
-				/>
-
-				<h2>Delicioso Pan Francés en Tijuana</h2>
-				<p>
-					Citrón es un café que se encuentra en avenida Alba Roja.
-				</p>
-				<p>
-					El lugar se distingue por su toque artístico, cada detalle importa, y esto se puede apreciar tanto en su atmósfera como en su cocina.
-				</p>
-				<p>
-					Cuentan con estacionamiento y no es necesario reservación.
-				</p>
-				<p>
-					<a href="https://www.instagram.com/citron.mx/" target="_blank" rel="nofollow noreferrer">@citron.mx</a>
-				</p>
-			</li>
+			<li><PanFrances /></li>
 		</ul>
 	</div>
 
