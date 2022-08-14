8511ca1873a2415d57e4cfa118705ec0e09c3764
jaime <garciadiazjaime@gmail.com>
2022-08-14 16:08:01 -0500

    refactor: cleanup

--- etl/support/save-to-json.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-function saveToJSON(filename, rawData) {
-  const data = JSON.stringify(rawData);
-
-  fs.writeFileSync(`./static/data/${filename}.json`, data);
-}
-
-module.exports = saveToJSON
